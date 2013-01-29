ab849e6042ea82b530538e500a6f204998af697a
Sergiohdz <sergiohdz92@gmail.com>
2013-01-29 07:06:14 -0800

    Create Pixeles.py

---
+++ Pixeles.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+from math import floor, sqrt
+
+  r = floor(sqrt(total /width * height)))
+  ra = width * r
+  rl = height * r
+  print 'Resolucion %d de %d pixeles' \
+  % (resWidth, resHeight)
