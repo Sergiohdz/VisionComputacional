729413b78f47be6373a32b80c239a1cd61258794
Sergiohdz <sergiohdz92@gmail.com>
2013-01-29 07:13:39 -0800

    Create Convertir.py

---
+++ Convertir.py
@@ -0,0 +1,10 @@
+#imagen.py
+#Cargar una imagen en Python
+
+from Tkinter import *
+from PIL import Image
+frame = frame()
+button.im = Image.open("imagen.jpg") 
+frame.pack()
+frame.mainloop()
+im.show()
