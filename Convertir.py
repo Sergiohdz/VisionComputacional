85b94edaab783b63751b9ffb7da2ef7ee0ae698a
Sergiohdz <sergiohdz92@gmail.com>
2013-02-04 23:43:15 -0600

    Update Convertir.py

--- Convertir.py
+++ Convertir.py
@@ -1,10 +1,22 @@
-#imagen.py
-#Cargar una imagen en Python
+#!/usr/bin/python
 
-from Tkinter import *
-from PIL import Image
-frame = frame()
-button.im = Image.open("imagen.jpg") 
-frame.pack()
-frame.mainloop()
-im.show()
+import pygame
+from pygame.locals import *
+import os, sys
+import Image
+
+im = Image.open("imagen.jpg")
+for x in range(0,900): #Ancho
+  for y in range(0,600): #Largo
+		pix = im.load()
+		
+pygame.init()
+ventana=pygame.display.set_mode((900,600))
+imagen=pygame.image.load("imagen.jpg")
+ventana.blit(imagen,(0,0))
+pygame.display.update()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            exit()
